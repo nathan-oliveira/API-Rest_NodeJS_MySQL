44d0ac146e4aceb3716c8de90b69602640fd2e8a
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-05-20 16:35:46 -0300

    api-rest

---
+++ bin/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+require('dotenv').config();
+const express = require('express');
+const bp = require('body-parser');
+const cors = require('cors');
+const consign = require('consign')
+
+const app = express();
+
+app.use(cors());
+
+app.use(bp.urlencoded({ extended: true }));
+app.use(bp.json({ limit: '20mb', extended: true }));
+
+app.use('/public', express.static(__dirname + '/public'));
+
+consign()
+  .include('src/routes')
+  .into(app)
+
+module.exports = app;
---
+++ src/controllers/client/ProductsController.js
@@ -0,0 +1,121 @@
+'use strict'
+
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+const ProductsDAO = require('../../models/client/ProductsDAO');
+const ResponseClass = require('../../models/ResponseClass');
+
+exports.list = (app, req, res, next) => {
+  let resp = new ResponseClass();
+
+  ProductsDAO.getAll(function(err, result) {
+    if(err) {
+      resp.error = true;
+      resp.msg = 'Ocorreu um erro!';
+    } else {
+      resp.dados = result;
+    }
+
+    res.json(resp);
+  })
+}
+
+exports.listById = (app, req, res, next) => {
+  let resp = new ResponseClass();
+
+  ProductsDAO.getId(req.params.id, function(err, result) {
+    if(err) {
+      resp.error = true;
+      resp.msg = 'Ocorreu um erro!';
+    } else {
+      resp.dados = result;
+    }
+
+    res.json(resp);
+  })
+}
+
+exports.toSave = (app, req, res, next) => {
+  let resp = new ResponseClass();
+
+  if(req.body.imagem != null) {
+    let bitmap = new Buffer.from(req.body.imagem.imagem_base64, 'base64');
+
+    let nome_img = `./public/uploads/${uuidv4()}-${req.body.imagem.nome_arquivo}`;
+
+    fs.writeFileSync(nome_img, bitmap);
+    req.body.caminho = nome_img;
+
+    ProductsDAO.toSave(req.body, function(err, result) {
+      if(err) {
+        resp.error = true;
+        resp.msg = 'Ocorreu um erro!';
+      } else {
+        if(result.affectedRows > 0) {
+          resp.msg = 'Cadastro realizado com sucesso!';
+        } else {
+          resp.error = true;
+          resp.msg = 'Não foi possivel efetuar o cadastro!';
+        }
+      }
+
+      res.json(resp);
+    })
+  } else {
+    resp.error = true;
+    resp.msg = 'Favor enviar uma imagem!';
+    res.json(resp);
+  }
+}
+
+exports.update = (app, req, res, next) => {
+  let resp = new ResponseClass();
+
+  if(req.body.imagem != null) {
+    let bitmap = new Buffer.from(req.body.imagem.imagem_base64, 'base64');
+
+    let nome_img = `./public/uploads/${uuidv4()}-${req.body.imagem.nome_arquivo}`;
+
+    fs.writeFileSync(nome_img, bitmap);
+    req.body.caminho = nome_img;
+  }
+
+  ProductsDAO.update(req.body, function(err, result) {
+    if(err) {
+      resp.error = true;
+      resp.msg = 'Ocorreu um erro!';
+    } else {
+      if(result.affectedRows > 0) {
+        resp.msg = 'Cadastro atualizado com sucesso!';
+      } else {
+        resp.error = true;
+        resp.msg = 'Não foi possivel atualizar o cadastro!';
+      }
+    }
+
+    res.json(resp);
+  })
+}
+
+exports.delete = (app, req, res, next) => {
+  let resp = new ResponseClass();
+
+  ProductsDAO.delete(req.params.id, function(err, result) {
+    if(err) {
+      resp.error = true;
+      resp.msg = 'Ocorreu um erro!';
+    } else {
+      if(result.affectedRows > 0) {
+        resp.msg = 'Cadastro excluido com sucesso!'
+      } else {
+        resp.error = true;
+        resp.msg = 'Não foi possivel excluir o cadastro!';
+      }
+
+      resp.dados = result
+    }
+
+    res.json(resp);
+  })
+}
